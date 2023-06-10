6a8d0b847c06ac64d2d9f97cde8cfc9edf72cfc7
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-10 09:53:55 +0530

    [sanjana] Add : Power of 2

--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -12,56 +12,16 @@ namespace SnakeAndLadder
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Calculator");
-            Console.WriteLine("----------------------------");
-            Console.WriteLine("1.Add");
-            Console.WriteLine("2.Substract");
-            Console.WriteLine("3.Multiply");
-            Console.WriteLine("4.Divide");
-
-            Console.Write("Enter Choice(1-4):");
-            int ch = Int32.Parse(Console.ReadLine());
-
-            int a, b, c;
-
-            switch (ch)
+            Console.WriteLine("Enter the power ");
+            double a= Convert.ToInt32(Console.ReadLine());
+            double result = 0;
+            for(int i = 0; i <=a; i++)
             {
-                case 1:
-                    Console.Write("Enter A:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter B:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    c = a + b;
-                    Console.WriteLine("Sum = {0}", c);
-                    break;
-                case 2:
-                    Console.Write("Enter A:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter B:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    c = a - b;
-                    Console.WriteLine("Difference = {0}", c);
-                    break;
-                case 3:
-                    Console.Write("Enter A:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter B:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    c = a * b;
-                    Console.WriteLine("Product = {0}", c);
-                    break;
-                case 4:
-                    Console.Write("Enter A:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter B:");
-                    b = Convert.ToInt32(Console.ReadLine());
-                    c = a / b;
-                    Console.WriteLine("Quotient = {0}", c);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Choice");
-                    break;
+                result = Math.Pow(2, i);
+                Console.WriteLine("2*"+i+"="+result);
             }
+            
+        
 
 
 
