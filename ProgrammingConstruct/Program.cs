e1191c459eb8634d952657b9695262bebb38eaa7
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-10 09:10:58 +0530

    [sanjana] Add : Two numbers are eqal or not

--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st No ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd No ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if(a==b)
+            {
+                Console.WriteLine("Two Numbers are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Two Numbers are not Equal");
+            }
+
+
+
+        }
+
+    }
+}
