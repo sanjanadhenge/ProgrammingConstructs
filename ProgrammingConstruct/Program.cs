bfb7c12d461306f690534b1d96d99422f10b7326
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-12 22:56:21 +0530

    [sanjana] Add : Sum of Five Natural number

--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -12,17 +12,13 @@ namespace SnakeAndLadder
         public static void Main(string[] args)
 
         {
-            string s = "", rev = "";
-            int len = 0;
-            Console.WriteLine("Enter a string");
-            s = Console.ReadLine();
-            len = s.Length - 1;
-            while (len >= 0)
+            int i = 1, sum = 0;
+            while(i<=5)
             {
-                rev = rev + s[len];
-                len--;
+                i++;
+                sum=sum+i;
             }
-            Console.WriteLine("Reverse of string is {0}", rev);
+            Console.WriteLine("Sum of Five Natural No is "+sum);
         }
 
     }
